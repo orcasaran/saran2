3d2e0995c13973e9378c483e5203946ebff00dab
orcasaran <saran.raj5474@gmail.com>
2014-08-25 17:44:51 +0530

    saran6

---
+++ c3exe6.c
@@ -0,0 +1,20 @@
+/* c3exe6 Using comments in a program ,
+Saran S, Aug 25, 2014 */
+
+#include <stdio.h>
+
+int main(void)
+{
+	//Declare the variables
+	int value1,value2,sum;
+	
+	//assign values and calculate their sum
+	value1=50;
+	value2=25;
+	sum=value1+value2;
+	
+	//display the result 
+	printf("The sum of %i and %i is %i\n",value1,value2,sum);
+
+	return 0;
+}
